cf0606815369fa37fd3d1d46e7eb7883aae5982f
lweikit <al.weikit@gmail.com>
2016-01-24 04:48:09 +0800

    Add: web-client

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngOpenFB'])
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services', 'ngOpenFB'])
 
 .run(function($ionicPlatform, ngFB) {
   ngFB.init({appId: '1542239316102873'});
