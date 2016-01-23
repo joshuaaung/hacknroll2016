08b02abe2257e0a6b19429c6dcc7cb68408469a9
lweikit <al.weikit@gmail.com>
2016-01-24 05:01:35 +0800

    Revert "Update:"
    This reverts commit 490b01394713b8dfb48294d800f25527779d9302.

--- www/js/app.js
+++ www/js/app.js
@@ -27,6 +27,13 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 
       StatusBar.styleDefault();
     }
     
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token) {
+      console.log("Device token:",token.token);
+    });
   });
 })
 
@@ -117,6 +124,6 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 
 
   // if none of the above states are matched, use this as the fallback
   //This is also a way to set the first page when the app launches
-  $urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/tab/dash'); 
 
 });
