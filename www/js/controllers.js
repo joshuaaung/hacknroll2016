646e2203fad8e710242486e619fd06d400ed7977
szenius <ting.szeying@gmail.com>
2016-01-23 23:50:32 +0800

    Merging changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -118,10 +118,7 @@ angular.module('starter.controllers', ['starter.services', 'ngOpenFB', 'ionic-da
   
 })
 
-.controller('StoresCtrl', function ($scope) {
-})
-
-.controller('ItemsCtrl', function ($rootScope, $scope, $state, $ionicLoading, Items, ngFB) {
+.controller('ItemsCtrl', function ($rootScope, $scope, $http, $state, $ionicLoading, Items, ngFB) {
   
   ngFB.api({
     path: '/me',
@@ -140,8 +137,38 @@ angular.module('starter.controllers', ['starter.services', 'ngOpenFB', 'ionic-da
     $ionicView.beforeEnter' will load first before entering to the page
   */
   $scope.$on('$ionicView.enter', function(e) {
-    $scope.items = Items.all();
-  });
++    //$scope.items = Items.all();
++    /*
++    $http({
++      method: 'GET',
++      url: 'http://experiment.thewhiteconcept.com/hackandroll/product/',
++      crossDomain : true,
++    }).then(function successCallback(response) {
++      // this callback will be called asynchronously
++      // when the response is available
++    }, function errorCallback(response) {
++      // called asynchronously if an error occurs
++      // or server returns response with an error status.
++    });
++    */
++    /* GOT CROSS-SITE DOMAIN ERROR */
++    //$http.get('http://experiment.thewhiteconcept.com/hackandroll/product/').then(function(resp) {
++    //  console.log('Success', resp);
++    //});
++    var url = 'http://experiment.thewhiteconcept.com/hackandroll/product/';
++    $http({ 
++      method: 'GET', 
++      url: url
++    }).then(function successCallback(resp) {
++      var jsonString = resp.data.substring(1, resp.data.length-1); //remove the first '(' and last ')' from the JSONP string
++      var jsonObject = JSON.parse(jsonString);
++      console.log(jsonObject);
++      $scope.items = jsonObject.products;
++      //handle_products(response);
++
++    }, function errorCallback(resp) {
++      console.log('Fail', resp);
++    });  });
   
   $scope.$on('$ionicView.beforeEnter', function(e) {
     $ionicLoading.show({
